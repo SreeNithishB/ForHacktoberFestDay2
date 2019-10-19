1f39446d6f0612c3911a2149dcd91c99b7eb4da7
SreeNithishB <sreenithishbofficial@gmail.com>
2019-10-19 19:10:19 +0530

    adding sum

--- calculator.c
+++ calculator.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-int sum(int a, int b) {
-	return a + b;
+int sum(int a, int b,int c) {
+	return a + b + c;
 }
 
 int main() {
 
 int a = 5,b = 10;
-printf("sum = %d", sum(a, b));
+printf("sum = %d", sum(a, b, c));
 
-printf("diff = %d",b - a);
+printf("diff = %d",diff(a, b));
 
 
 return 0;
